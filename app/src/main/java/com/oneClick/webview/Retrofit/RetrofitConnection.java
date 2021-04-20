730132095f88f9688618a7528f431660a9d20127
tazzaenciel <tazzaenciel@gmail.com>
2021-04-20 16:01:17 +0900

    update

--- app/src/main/java/com/oneClick/webview/Retrofit/RetrofitConnection.java
+++ app/src/main/java/com/oneClick/webview/Retrofit/RetrofitConnection.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitConnection {
-    String URL = "https://oneclick-stock.com/";
+    String URL = "http://refund.willsoft.kr/";
 
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(URL)
